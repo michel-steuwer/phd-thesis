0f68580c876ef25531bf31638327a39c2cebb5c5
Michel Steuwer <michel.steuwer@uni-muenster.de>
2015-03-25 21:46:02 +0000

    Added plot template for SkelCL dotproduct plot.

---
+++ Figures/Plots/DotProduct/dotProduct.R
@@ -0,0 +1,32 @@
+require('ggplot2')
+require('grid')
+require('reshape2')
+
+# row.names = NULL forces numbering of the lines and, thus, preserves the order
+performance <- read.csv("dotProduct.csv", sep=";", comment.char="#", header=TRUE, row.names=NULL)
+performance <- melt(performance)
+colnames(performance) <- c("Size", "Versions", "value")
+
+rownumbers <- as.integer(row.names(performance))
+
+# ====Default settins====
+theme <- theme_bw() +
+  theme(text = element_text(size = 22),
+        axis.title = element_text(size = rel(1.125), vjust = 1),
+        axis.text.x = element_text(angle = 45, vjust = 0.5),
+        panel.border = element_rect(linetype = "solid", colour = "black", size = 1.5),
+        plot.margin = unit(c(2,0.5,0,1), 'cm'))
+
+labs <- labs(x = "Data Size", y = "Runtime in ms\n(log scale)")
+
+# create plot
+plot <- ggplot(performance, aes(x = reorder(Size, rownumbers), y = value, group=Versions)) +
+  geom_line(aes(color = Versions)) +
+  scale_y_log10(expand = c(0,0), limits = c(1,1000)) +
+  scale_x_discrete(expand=c(0,0.1)) +
+  labs + theme
+
+# save plot to file
+ggsave(file = "dotProduct.pdf", width = 9, height = 6)
+
+plot
